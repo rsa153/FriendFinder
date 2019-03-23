8f7866c6dd1bdb57d578263d5cc0f6367733fa0f
rsa153 <r-saber@nlaw.northwestern.edu>
2019-03-23 15:52:09 -0500

    used prettier to clean up code

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,20 +2,19 @@
 var path = require("path");
 
 // Export HTML routes
-module.exports = function (app) {
-
+module.exports = function(app) {
   //Home Page
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'));
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   //Survey Page
-  app.get("/survey", function (req, res) {
+  app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
 // Tells node that we are creating an express server
 var app = express();
 
-// Sets an initial port. 
+// Sets an initial port.
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
@@ -16,6 +16,6 @@ require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 // Start listening on the Port
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
